f018d67c1c0dee6c323749265370e15c94dc0bb4
edbbioeco <edsonbbiologia@gmail.com>
2026-04-21 09:41:58 -0300

    Visualizar dados

--- script_consertado_atividade_5.R
+++ script_consertado_atividade_5.R
@@ -38,3 +38,9 @@ importar_dados <- function(dados){
 }
 
 purrr::map(dados, importar_dados)
+
+## Visualizar ----
+
+dados |>
+  stringr::str_remove_all("dados/|.txt") |>
+  mget(envir = globalenv())
