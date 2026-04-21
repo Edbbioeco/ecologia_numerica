95a595c30a180288d1e0989c23d5aa8c96a10364
edbbioeco <edsonbbiologia@gmail.com>
2026-04-21 09:31:45 -0300

    Comandos para importar os dados

--- script_consertado_atividade_5.R
+++ script_consertado_atividade_5.R
@@ -13,3 +13,28 @@ library(ggbeeswarm)
 library(ggview)
 
 library(magrittr)
+
+# Dados ----
+
+## Importar ----
+
+dados <- list.files(path = "dados/",
+                    full.names = TRUE) |>
+  stringr::str_subset("FlorestaTot|macrofauna")
+
+dados
+
+importar_dados <- function(dados){
+
+  dados_importados <- readr::read_table(dados)
+
+  nome <- dados |>
+    stringr::str_remove_all("dados/|.txt")
+
+  assign(nome,
+         dados_importados,
+         envir = globalenv())
+
+}
+
+purrr::map(dados, importar_dados)
