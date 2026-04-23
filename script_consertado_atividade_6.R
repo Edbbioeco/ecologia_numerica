cae57ea5d0e6a2e4009034eb7aeafbebd51f6ff8
Edbbioeco <edsonbbiologia@gmail.com>
2026-04-23 16:23:54 -0300

    Remover comunidades com rieuza < 2

--- script_consertado_atividade_6.R
+++ script_consertado_atividade_6.R
@@ -20,6 +20,19 @@ riqueza |> dplyr::glimpse()
 
 # Curva de abundância ----
 
+## Remover comunidades com riqueza < 2 ----
+
+riqueza <- riqueza |>
+  dplyr::mutate(riqueza = riqueza |>
+                  tibble::column_to_rownames(var = "sp1") |>
+                  vegan::specnumber()) |>
+  dplyr::filter(riqueza > 1) |>
+  dplyr::select(-riqueza)
+
+riqueza
+
+riqueza |> dplyr::glimpse()
+
 ## Calcular modelos -----
 
 modelos_curvas <- riqueza |>
